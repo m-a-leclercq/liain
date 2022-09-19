815ff55774fef91f5e35ab1000fa4b0f48e58f9d
LECLERCQ Marc-Antoine <marcantoine.leclercq@orange.com>
2022-09-19 22:49:54 +0200

    Cleanup templates, filebeat now a service. All files to /var/log/liain.

--- dl_schedule.sh
+++ dl_schedule.sh
@@ -2,16 +2,8 @@
 # Cleanup on aisle two!
 rm -rf /var/log/liain/*
 
-
 # Time to get this show on the road
 source /root/venv_liain/bin/activate
 
 echo 'running python'
-
-if python3 /root/liain/liain.py; then
-    echo 'running filebeat'
-    filebeat -e -c liain.yml --path.data /var/lib/filebeat_liain/
-else
-    echo 'FAIL'
-fi
-
+python3 /root/liain/liain.py
--- liain.py
+++ liain.py
@@ -8,7 +8,6 @@ URL = "https://operateurs.liain.fr/ipe/"
 
 def search_ipe_file():
     # Scan all available files, grab IPE archive filename
-
     req = requests.get(URL)
     filename = BeautifulSoup(req.text, 'html.parser').find(href=re.compile("LIAIN_01_SIEA.*")).string
 
@@ -19,15 +18,14 @@ def download_file(filename):
     dl_url = "{0}{1}".format(URL, filename)
     download = requests.get(dl_url, stream=True)
 
-    with open(filename, 'wb') as fd:
+    with open("/var/log/liain/{}".format(filename), 'wb') as fd:
         for chunk in download.iter_content(chunk_size=128):
             fd.write(chunk)
 
 def unzip_file(zip_filename):
-    with zipfile.ZipFile(zip_filename, "r") as zip_ref:
+    with zipfile.ZipFile("/var/log/liain/{}".format(zip_filename), "r") as zip_ref:
         zip_ref.extractall("/var/log/liain/")
 
 filename = search_ipe_file()
 download_file(filename)
 unzip_file(filename)
-
